ba96583b99202f8d1a0cc82aa88e71026026d9ff
Dmitry Polyanitsa <d.polyanitsa@criteo.com>
2016-01-12 15:46:17 +0100

    Cache projects to speedup the trigger startup
    Change-Id: I042658449bfe578f0c93f71a9ab4cee81680cb06

--- src/main/java/org/jenkinsci/plugins/gitlab/Gitlab.java
+++ src/main/java/org/jenkinsci/plugins/gitlab/Gitlab.java
@@ -1,17 +1,23 @@
 package org.jenkinsci.plugins.gitlab;
 
 import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.models.GitlabProject;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * GitlabAPI Wrapper Class
  */
 public class Gitlab {
     private GitlabAPI _api;
+    private ProjectsCache _projectsCache;
 
     private void connect() {
         String privateToken = GitlabBuildTrigger.getDesc().getBotApiTokenSecret().getPlainText();
         String apiUrl = GitlabBuildTrigger.getDesc().getGitlabHostUrl();
         _api = GitlabAPI.connect(apiUrl, privateToken);
+        _projectsCache = new ProjectsCache(_api, 5 * 60 * 1000 /* 5 minutes */);
     }
 
     public GitlabAPI get() {
@@ -20,5 +26,34 @@ public class Gitlab {
         }
 
         return _api;
+    }
+
+    public List<GitlabProject> getCachedProjects() throws IOException {
+        return _projectsCache.get();
+    }
+
+    private static class ProjectsCache {
+        private final GitlabAPI _api;
+        private final int _timeToLiveMs;
+
+        private volatile long _lastUpdateTimeMs;
+        private volatile List<GitlabProject> _projects;
+
+        public ProjectsCache(GitlabAPI api, int timeToLiveInMilliseconds) {
+            _api = api;
+            _timeToLiveMs = timeToLiveInMilliseconds;
+        }
+
+        public List<GitlabProject> get() throws IOException {
+            if (_projects == null || (System.currentTimeMillis() - _lastUpdateTimeMs) > _timeToLiveMs) {
+                synchronized (this) {
+                    if (_projects == null || (System.currentTimeMillis() - _lastUpdateTimeMs) > _timeToLiveMs) {
+                        _projects = _api.getAllProjects();
+                        _lastUpdateTimeMs = System.currentTimeMillis();
+                    }
+                }
+            }
+            return _projects;
+        }
     }
 }
